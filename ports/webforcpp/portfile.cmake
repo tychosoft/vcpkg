b48bf593eb4b34aa8bcbfb35c3a9fb7450024fd4
David Sugar <tychosoft@gmail.com>
2024-12-10 07:38:53 -0500

    Fix webforcpp port

--- ports/webforcpp/portfile.cmake
+++ ports/webforcpp/portfile.cmake
@@ -2,8 +2,8 @@ vcpkg_from_gitlab(
     GITLAB_URL https://gitlab.com
     OUT_SOURCE_PATH SOURCE_PATH
     REPO tychosoft/webforcpp
-    REF v0.1.2
-    SHA512 e6b53284a4813a21de6a80f550306304b2571106f54acb1dc38fd1cd470a01add0964e58254f5f97df4eab7b3ebcb4b3b5aad1ce6d224f24fa5d91a88f80271f
+    REF v0.1.3
+    SHA512 50bd14f759e1a6534a0c69f5061942c08438740c95119da4d1a34baaec46c9a1a9688a1c9ef4051515d799c3cc0be4efe71d9e4ba643d73d1efdea0b4cb66f26
     HEAD_REF main
 )
 
